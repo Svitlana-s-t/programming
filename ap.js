6f0af0776f2d16a2dba9bbd3c75f6f5e3a2f86e8
WEB-20-1-1 <74668645+WEB-20-1-1@users.noreply.github.com>
2021-07-12 15:25:21 +0300

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,10 @@
+let name1 = prompt('Введите ваше имя?');
+alert(name1)
+let name2 = prompt('Введите вашу фамилию?');
+alert(name2)
+let name3 = prompt('Введите ваш возвраст?');
+alert(name3)
+
+
+
+
